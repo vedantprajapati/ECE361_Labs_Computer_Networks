f3f843bd2425a8338c5349d267c6f083d5dcd4bf
Vedant Prajapati <vedant.prajapati@mail.utoronto.ca>
2022-09-26 20:32:15 -0400

    string2binary function

--- filetransfer/02/deliver.c
+++ filetransfer/02/deliver.c
@@ -58,9 +58,26 @@ char* formString(struct packet packet){
     strcat(packetString,":");
     strcat(packetString,packet.filename);
     strcat(packetString,":");
-    strcat(packetString,packet.filedata);
     
     return packetString;
+}
+
+char* stringToBinary(char* s) {
+    if(s == NULL) return 0; /* no input string */
+    size_t len = strlen(s);
+    char *binary = malloc(len*8 + 1); // each char is one byte (8 bits) and + 1 at the end for null terminator
+    binary[0] = '\0';
+    for(size_t i = 0; i < len; ++i) {
+        char ch = s[i];
+        for(int j = 7; j >= 0; --j){
+            if(ch & (1 << j)) {
+                strcat(binary,"1");
+            } else {
+                strcat(binary,"0");
+            }
+        }
+    }
+    return binary;
 }
 
 int main(int argc, char *argv[]){
@@ -73,6 +90,7 @@ int main(int argc, char *argv[]){
     int fileFound = -1;
     char dataBuffer[32];
     char *filename;
+    char* binaryString; 
 
     time_t begin, end;
 
@@ -147,6 +165,7 @@ int main(int argc, char *argv[]){
         fileTranPacket.frag_no = n;
         fragment(file_to_send, fileTranPacket.filedata);
         packetString = formString(fileTranPacket);//fileTranPacket.total_frag + ":" + fileTranPacket.frag_no + ":" + fileTranPacket.size + ":" + fileTranPacket.filedata;
+        binaryString = stringToBinary(packetString);
         n++;
     }
     
