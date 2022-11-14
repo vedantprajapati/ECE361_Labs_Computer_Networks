9aba3c223b4c10ae3a8a03098c0914b70bcc300d
Danny Ahmad <43858798+daniql@users.noreply.github.com>
2022-11-14 14:51:56 -0500

    roughed in a bit more

--- textconferencing/01/deliver/deliver.c
+++ textconferencing/01/deliver/deliver.c
@@ -11,51 +11,55 @@
 #include <math.h>
 #include <stdbool.h>
 
-#define MAX_PACKET_SIZE 1000
-#define RECV_PACKET_SIZE 1100
-#define MAX_ATTEMPTS 3
+#define MAX_DATA 1000
+#define MAX_NAME 12
 #define PORT 8080
+#define BUFFER_SIZE 1024
 
-// packet format: "total_frag:frag_no:size:filename:filedata"
-struct packet
-{                            // packet format: "total_frag:frag_no:size:filename:filedata"
-    unsigned int total_frag; // total number of fragments of the file
-    unsigned int frag_no;    // sequence number of fragment
-    unsigned int size;       // size of data, range [0,1000]
-    char *filename;
-    char filedata[MAX_PACKET_SIZE];
+
+struct message
+{                       
+    unsigned int type; 
+    unsigned int size; 
+    unsigned char source[MAX_NAME];
+    unsigned char data[MAX_DATA];
 };
 
-void textApp(int sock){
+void textApp(int sockfd){
+    char input_buffer[BUFFER_SIZE];
     while(1){
-
+        bzero(input_buffer, BUFFER_SIZE);
+        
     }
 }
 
 int main(int argc, char *argv[])
 {
-    int sock, conn;
-    struct sockaddr_in server, client;
+    if(argc > 1){
+        printf("usage: client\n");
+        exit(0);
+    }
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock < 0){
+    int sockfd, conn;
+    struct sockaddr_in serv_addr, client;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0){
         printf("socket creation error\n");
         exit(0);
     }
 
-    bzero(&server, sizeof(server));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_port = htons(PORT);
 
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_port = htons(PORT);
-
-    if(connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0){
+    if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
         printf("connection error\n");
         exit(0);
     }
 
-    textApp(sock);
+    textApp(sockfd);
 
-    close(sock);
+    close(sockfd);
     return 0;
 }
--- textconferencing/01/server/server.c
+++ textconferencing/01/server/server.c
@@ -10,14 +10,85 @@
 #include <arpa/inet.h>
 #include <time.h>
 
-void textApp(int conn){
+#define MAX_DATA 1000
+#define MAX_NAME 12
+#define BUFFER_SIZE 1024
+#define BACKLOG 3
+//packet format = type:size:source:data
+struct message
+{                       
+    unsigned int type; 
+    unsigned int size; 
+    unsigned char source[MAX_NAME];
+    unsigned char data[MAX_DATA];
+};
+
+void textApp(int connfd){
+    char input_buffer[BUFFER_SIZE];
+    struct message recvd_packet;
     while(1){
+        bzero(input_buffer, BUFFER_SIZE);
+
+        if(read(connfd, input_buffer, BUFFER_SIZE)<0){
+            printf("read error\n");
+            exit(0);
+        }
+        
+        printf("%s",input_buffer);
+        int i = 0;
+        char *token = strtok(input_buffer, ":");
+
+        while(token != NULL){
+            switch(i){
+                case 0:
+                    recvd_packet.type = token;
+                    i++;
+                    token = strtok(NULL, input_buffer);
+                    break;
+                case 1:
+                    recvd_packet.size = atoi(token);
+                    i++;
+                    token = strtok(NULL, input_buffer);
+                    break;
+                case 2:
+                    strcpy(recvd_packet.source,token);
+                    i++;
+                    token = strtok(NULL, input_buffer);
+                    break;
+                case 3:
+                    strcpy(recvd_packet.data,token);
+                    token = strtok(NULL, input_buffer);
+                    break;
+                default:
+                    printf("packet format error - too many segments\n");
+                    exit(0);
+            }
+        }
+
+        if(i != 3){
+            printf("packet format error - too few segments\n");
+            exit(0);
+        }
+
+        switch(recvd_packet.type){
+            case 0://login
+            case 1://exit
+            case 2://join
+            case 3://leave_session
+            case 4://new_session
+            case 5://message
+            case 6://query
+            default:
+                printf("unknown type error\n");
+                exit(0);
+        }
+
     }
+    close(connfd);
 }
 
 int main(int argc, char const *argv[])
 {
-
     if (argc != 2)
     {
         printf("usage: server <TCP port to listen on>\n");
@@ -26,37 +97,40 @@ int main(int argc, char const *argv[])
 
     int port = atoi(argv[1]);
 
-    int sock, conn, len;
-    struct sockaddr_in server, client;
+    int sockfd, connfd;
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t len;
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-
-    if(sock < 0){
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0){
         printf("socket creation error\n");
         exit(0);
     }
 
-    bzero(&server, sizeof(server));
+    serv_addr.sin_family = AF_INET6;
+    serv_addr.sin_addr.s_addr = INADDR_ANY; //constant used to store any IP address assigned to server
+    serv_addr.sin_port = htons(port);
 
-    server.sin_family = AF_INET6;
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = htons(port);
-
-    if(bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0){
+    if(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
         printf("bind error\n");
         exit(0);
     };
 
-    if(listen(sock, 3) != 0){
+    if(listen(sockfd, BACKLOG) != 0){
         printf("listen error\n");
         exit(0);
     }
 
-    len = sizeof(client);
-    conn = accept(sock, (struct sockaddr *)&client, &len);
+    len = sizeof(cli_addr);
+    connfd = accept(sockfd, (struct sockaddr *)&cli_addr, &len);
 
-    textApp(conn);
+    if(connfd < 0){
+        printf("accept error\n");
+        exit(0);
+    }
 
-    close(sock);
+    textApp(connfd);
+
+    close(sockfd);
     return 0;
 }
